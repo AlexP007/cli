02424e67381311bde9ccf5f3a24c17a9512eb606
AlexP <alex.p.panteleev@gmail.com>
2020-07-03 20:41:26 +0300

    [i] upgrade CommandTest

--- tests/Domain/CommandTest.php
+++ tests/Domain/CommandTest.php
@@ -4,10 +4,21 @@
 use PHPUnit\Framework\TestCase;
 
 use Cli\Domain\Command;
+use Cli\Basic\Environment;
 
 class CommandTest extends TestCase
 {
     const SIMPLE_COMMAND_NAME = 'command';
+
+    /**
+     * @var callable
+     */
+    private $simpleFunc;
+
+    /**
+     * @var Environment
+     */
+    private $simplyEnvironment;
 
     /**
      * @expectedException Cli\Exception\ArgumentException
@@ -21,14 +32,20 @@ class CommandTest extends TestCase
     {
         $command = $this->newSimpleCommand(self::SIMPLE_COMMAND_NAME);
         $this->assertInstanceOf(Command::class, $command);
+
+        $this->assertEquals(self::SIMPLE_COMMAND_NAME, $command->getName());
+        $this->assertEquals($this->simpleFunc, $command->getCallable());
+        $this->assertEquals($this->simpleFunc, $command->getCallable());
+        $this->assertEquals([], $command->getFlags());
+        $this->assertEquals($this->simplyEnvironment, $command->getEnv());
     }
 
     private function newSimpleCommand($name)
     {
-        $func = function($param1, $param2) {
+        $this->simpleFunc = function($param1, $param2) {
             return $param1 . $param2;
         };
-
-        return new Command($name, $func, [], []);
+        $this->simplyEnvironment = new Environment([]);
+        return new Command($name, $this->simpleFunc, [], $this->simplyEnvironment);
     }
 }
